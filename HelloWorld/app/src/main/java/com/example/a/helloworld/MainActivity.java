1e7f58e053e15128a47c389db1007119fc1450ef
workflowAndy <workflow@daum.com>
2017-02-06 15:02:34 +0900

    first commit

---
+++ HelloWorld/app/src/main/java/com/example/a/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClicked(View v){
+        Toast.makeText(this, "button click", Toast.LENGTH_SHORT).show();
+    }
+}
