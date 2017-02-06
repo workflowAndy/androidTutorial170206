06b70fc7dabd9b511da3f8d15a4f3762aa21f1d9
workflowAndy <workflow@daum.com>
2017-02-06 16:50:20 +0900

    radio button

--- 02_Widget2/app/src/main/java/com/example/a/a02_widget2/MainActivity.java
+++ 02_Widget2/app/src/main/java/com/example/a/a02_widget2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a.a02_widget2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radio1:
+                        Toast.makeText(MainActivity.this, "radio1 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio2:
+                        Toast.makeText(MainActivity.this, "radio2 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio3:
+                        Toast.makeText(MainActivity.this, "radio3 selected", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+            }
+        });
+
+
     }
 }
