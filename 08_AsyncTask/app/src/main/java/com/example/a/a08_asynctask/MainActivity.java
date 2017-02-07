ed49168a2a5920c7ed2c9ff998f26eda9097afe8
workflowAndy <workflow@daum.com>
2017-02-07 14:59:07 +0900

    async

--- 08_AsyncTask/app/src/main/java/com/example/a/a08_asynctask/MainActivity.java
+++ 08_AsyncTask/app/src/main/java/com/example/a/a08_asynctask/MainActivity.java
@@ -4,18 +4,41 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView myTextView;
+
     class MyAsyncTask extends AsyncTask<Integer,Float,String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            myTextView.setText(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(Float... values) {
+            super.onProgressUpdate(values);
+            myTextView.setText("count:" + values[0]);
+        }
 
         @Override
         protected String doInBackground(Integer... params) {
             int startValue = params[0];
-            for(;startValue < 100;startValue++){
-                Log.d("AsysncTask","count" + startValue);
+            for(;startValue < 50;startValue++){
+                Log.d("Async","count" + startValue);
+                publishProgress((float)startValue); //onProgressUpdate 을 호출한다
+                //myTextView.setText("count:" + startValue);
+
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -29,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView = (TextView) findViewById(R.id.myTextView);
 
         MyAsyncTask myAsyncTask = new MyAsyncTask();
         myAsyncTask.execute(30);
